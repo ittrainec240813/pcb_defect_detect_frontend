2249dc8b028208b7a28c7d736b7ca7e6a4f00d28
Luke3501 <lukechu3501@gmail.com>
2024-10-28 16:00:04 +0800

    add navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,57 @@
+"use client"
+
+import { useState } from "react";
 import "./globals.css";
+import AppHeader from "@/components/AppHeader";
+import MainDrawer from "@/components/MainDrawer";
+import { Box, styled } from "@mui/material";
+
+interface MainContainerProps {
+  open?: boolean;
+}
+
+const MainContainer = styled(Box)<MainContainerProps>(({ theme }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        width: "80%",
+        marginLeft: "20%",
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+  display: "flex",
+  flexDirection: "column",
+  width: "100%"
+}))
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [showDrawer, setShowDrawer] = useState<boolean>(false);
+
+  const handleDrawer = () => {
+    setShowDrawer(prev => !prev);
+  }
+
   return (
     <html lang="en">
       <body>
-        {children}
+        <MainDrawer open={showDrawer} onSetDrawer={handleDrawer}/>
+        <MainContainer open={showDrawer}>
+          <AppHeader onSetDrawer={handleDrawer} />
+          {children}
+        </MainContainer>
       </body>
     </html>
   );
---
+++ components/MainDrawer.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { 
+  Divider,
+  Drawer,
+  DrawerProps,
+  IconButton,
+  List, 
+  ListItem, 
+  ListItemButton, 
+  ListItemText,
+  styled
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import HomeIcon from '@mui/icons-material/Home';
+import ChatIcon from '@mui/icons-material/Chat';
+
+interface MainDrawerProps extends DrawerProps {
+  open: boolean;
+  onSetDrawer: () => void;
+}
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+const MainDrawer:React.FC<MainDrawerProps> = ({ open, onSetDrawer }) => {
+  const router = useRouter();
+
+  const routes = [
+    {
+      title: "Home",
+      icon: <HomeIcon sx={{color: "white", mr: "1px"}} />,
+      route: "/"
+    },
+    {
+      title: "Chat",
+      icon: <ChatIcon sx={{color: "white", mr: "1px"}} />,
+      route: "/chat"
+    }
+  ]
+
+  const handleRouteChange = (newRoute: string) => {
+    onSetDrawer();
+    router.push(newRoute);
+  }
+
+  return (
+    <Drawer 
+      open={open} 
+      sx={{
+        width: "20%",
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: "20%",
+        
+          bgcolor: "#1d1d1d"
+          // boxSizing: 'border-box',
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+    >
+      <List 
+        sx={{width: "100%" }}
+      >
+        <DrawerHeader>
+          <IconButton onClick={onSetDrawer}>
+            <ChevronLeftIcon sx={{color: "white"}}/>
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        {routes.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton onClick={() => handleRouteChange(item.route)} >
+              {item.icon}
+              <ListItemText sx={{color: "#FFFFFF"}} primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  )
+}
+
+export default MainDrawer;
